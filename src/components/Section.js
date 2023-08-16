e0268b0eaff133d851561f723fdb4bfc5c226db0
Formazione Quinck <formazionequinck@MBPdiFomazione2.homenet.telecomitalia.it>
2023-08-16 14:53:15 +0200

    open API

--- src/components/Section.js
+++ src/components/Section.js
@@ -2,15 +2,18 @@ import React from 'react';
 import LineCards from './LineCards';
 import '../App.css';
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+const y  = getRandomInt(14)
+console.log(y)
 
 function Section(props){
+
   const sectionContent = [];
-    for (let i = 0 ; i < 6 ; i += 2){
-      let x = i + 1
-      console.log(i)
-      console.log(x)
+    for (let i = y ; i < y + 6; i += 2){
       sectionContent.push(
-        <LineCards characterData1={props.characterData[i]} characterData2={props.characterData[x]} key={i}/>
+        <LineCards characterData1={props.characterData[i]} characterData2={props.characterData[i+1]} key={i}/>
       );
     }     
     return(
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,16 +1,34 @@
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header.js';
 import Footer from '../components/Footer.js';
 import Section from '../components/Section.js';
 import Titolo from '../components/Titolo.js';
-import characterData from '../assets/characterData.js';
-import '/Users/formazionequinck/Desktop/workspace/react-quick-start/src/App.css';
+import '../App.css';
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+let x  = getRandomInt(42)
+
 
 export default function Homepage() {
+    const [characters, setCharacters] = useState();
+    useEffect(() => {
+      fetch(`https://rickandmortyapi.com/api/character/?page=${x}`)
+        .then((response) => response.json())
+        .then((data) => {
+            setCharacters(data.results)
+        })
+        .catch((err) => {
+            console.log(err.message)
+        });
+    }, []);
+
     return (
     <div>
       <Header/>
       <Titolo/>
-      <Section characterData={characterData}/>
+      {characters && <Section characterData={characters}/>}
       <Footer/>
     </div>
   );
